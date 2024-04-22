0742c39cfbc5722069f4009c925450dc3f78eb69
ken8806 <71235937+ken8806@users.noreply.github.com>
2024-04-22 13:22:06 +0200

    Adds more functionality from coaching session

--- index.js
+++ index.js
@@ -28,17 +28,46 @@ document.addEventListener('DOMContentLoaded', function(){
     function displayList(){
         taskListEl.textContent = " ";
         for(let i = 0; i < taskList.length ; i++){
-          console.log(`${ i + 1 }. ${taskList[i]}`)
+          
           const newTask = document.createElement('li');
-          newTask.innerHTML = `${taskList[i].task} ${taskList[i].date} <input  id="checkbox${i}" type="checkbox" onchange="${checkTask(this.id)}" value="${taskList[i].completed}"/>`;
-            
-          taskListEl.appendChild(newTask);
+
+          //newTask.innerHTML = `${taskList[i].task} ${taskList[i].date} <input  id="checkbox${i}" type="checkbox" value="${taskList[i].completed}"/>`;
+
+          newTask.innerText = `${taskList[i].task} ${taskList[i].date}`
+
+          const inputEl = document.createElement("input")
+          inputEl.type = "checkbox"
+          inputEl.id = `checkbox${i}`
+          inputEl.checked = taskList[i].completed
+
+          inputEl.addEventListener("change", function(event){
+            checkTask(i, event)
+          })
+        
+          newTask.appendChild(inputEl);
+
+          taskListEl.appendChild(newTask)
+
+          /*const checkboxEl = document.getElementById(`checkbox${i}`)
+          checkboxEl.addEventListener("change", function(){
+            checkTask(`checkbox${i}`)
+          })*/
+
         } 
     };
 
-    function checkTask (id){
-       console.log("Id =", id);
+    function checkTask (index, event){
+
+        const currentTask = taskList[index]
+    
+        currentTask.completed = event.target.checked
+        console.log(currentTask)
+
+        
+        addToLocalStorage(taskList)
+      
     }
+
     function addToLocalStorage (taskList) {
         localStorage.setItem("tasks", JSON.stringify(taskList));
     };
